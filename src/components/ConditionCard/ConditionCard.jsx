c2f261af6da5c4d33c43c04ae8791cd5c01bbee3
stephtalbott <talbottstephanie@ymail.com>
2023-06-27 15:23:16 -0500

    styles cards in city details

--- src/components/ConditionCard/ConditionCard.jsx
+++ src/components/ConditionCard/ConditionCard.jsx
@@ -1,12 +1,11 @@
 export default function ConditionCard({condition}){
     return (
         <>
-            <div className='feels-like-container'>
-                <div className='feels-like-card'>
-                    <h1 className='card-title'>{condition.text}</h1>
-                    <img src={condition.icon}/>
-                </div>
+            <div className='bg-blue-500 rounded-lg p-4 w-32 h-32'>
+                <h1 className='text-lg font-semibold mb-2'>{condition.text}</h1>
+                <img src={condition.icon}/>
             </div>
         </>
     )
 }
+
