f3cfba8d47554eaa92976a4da51155d3398ee79a
lmikszta1 <lmikszta@gmail.com>
2023-06-28 10:41:50 -0400

    additional styling

--- src/components/ConditionCard/ConditionCard.jsx
+++ src/components/ConditionCard/ConditionCard.jsx
@@ -1,9 +1,9 @@
 export default function ConditionCard({condition}){
     return (
         <>
-            <div className='bg-blue-500 rounded-lg p-4 w-32 h-32 flex justify-center items-center mx-auto'>
+            <div className='bg-gradient-to-b from-blue-200 to-blue-600 rounded-lg p-4 w-32 h-32 flex justify-center items-center mx-auto'>
                 <h1 className='font-semibold mb-2 text-sm'>{condition.text}</h1>
-                <img src={condition.icon}/>
+                <img alt="condition icon" src={condition.icon}/>
             </div>
         </>
     )
