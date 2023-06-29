b3fa87a07c0abab1619873d11e352e8dd51faa38
lmikszta1 <lmikszta@gmail.com>
2023-06-29 15:09:52 -0400

    changes font size for condition card conditionally

--- src/components/ConditionCard/ConditionCard.jsx
+++ src/components/ConditionCard/ConditionCard.jsx
@@ -1,11 +1,12 @@
 import Card from 'react-bootstrap/Card'
+import "./ConditionCard.css"
 
 export default function ConditionCard({condition}){
     return (
         <>
             <Card style={{ width: '8rem', height: '8rem'}} bg="primary" border="dark">
                 <Card.Body>
-                    <Card.Title className='fw-bold fs-5'>{condition.text}</Card.Title>
+                    <Card.Title className={condition.text.includes('thunder') ? 'fw-bold fs-8' : 'fw-bold fs-6'}>{condition.text}</Card.Title>
                     <Card.Text>
                         <img alt="condition icon" src={condition.icon}/>
                     </Card.Text>
